c2433db9591471e30b95ea0873be1a0fd3afce87
kaddlz <roslanov@icloud.com>
2026-03-16 19:45:53 +0300

    Split `owners` table into `owners_basic` and `owners_contact` for normalized data structure.

---
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerContact.java
@@ -0,0 +1,62 @@
+package org.springframework.samples.petclinic.owner;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "owners_contact")
+public class OwnerContact {
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+	@Id
+    private Owner owner;
+
+    @Size(max = 255)
+    @Column(name = "address")
+	@NotBlank
+    private String address;
+
+    @Size(max = 30)
+    @Column(name = "city", length = 30)
+	@NotBlank
+    private String city;
+
+    @Size(max = 20)
+    @Column(name = "telephone", length = 20)
+	@NotBlank
+    private String telephone;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+}
